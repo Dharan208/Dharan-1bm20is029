88b7286aceeaec0560aa213d648202787b9ef408
Dharan208 <84971183+Dharan208@users.noreply.github.com>
2024-01-02 12:10:34 +0530

    Update server.py

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ app = Flask(_name_)
 
 @app.route("/")
 def hello():
-    return "Hello, Dew Drop Service!"
+    return "Hello,My Name is Dharan Pramod 1BM20IS029"
 
 if _name_ == "_main_":
     app.run(host='0.0.0.0')
